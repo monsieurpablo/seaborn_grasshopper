285a1f3b6317c25c1b50dd0ee5db393e8faadc69
Pablo Arango <pabloarango97@gmail.com>
2023-01-18 20:40:57 +0000

    update READ me and fix issue with TK  in matplot

--- README.md
+++ README.md
@@ -1,32 +1,95 @@
 # Seaborn Grasshopper
-A Rhino Remote library that brings Seaborn (Matplotlib) inside of Grasshopper using [Hops](https://github.com/mcneel/compute.rhino3d/tree/master/src/ghhops-server-py). 
+
+A Rhino Remote library that brings Seaborn (Matplotlib) inside of Grasshopper using [Hops](https://github.com/mcneel/compute.rhino3d/tree/master/src/ghhops-server-py).
 
 This second version is a complete refactor of the seaborn-grasshopper implementation. This approach greatly improves from the first version in the following ways:
+
 - Additional fuctions added
-    - `despine`     Dictionary with "Despine" functions arguments, that removes top and right axis lines. 
-    - `add_args`    Use additional figure arguments are not implemented in Grasshopper. 
-    - `ax_args`     Implement axis arguments to the matplotlib.Axis element. (Set title, x and y limits, etc ) 
-    - `fig_size`    Change figure size
+  - `despine` Dictionary with "Despine" functions arguments, that removes top and right axis lines.
+  - `add_args` Use additional figure arguments are not implemented in Grasshopper.
+  - `ax_args` Implement axis arguments to the matplotlib.Axis element. (Set title, x and y limits, etc )
+  - `fig_size` Change figure size
 - Ready for web deployment using Heroku and gunicorn
 - Export image as a encoded b64 string
 - Less code duplication
 - Correct handling of defaults
 - Style improvement
 
-
-## More information
-More infromation on how to use the tool [comming soon](). 
-
 ## Typical workflow
+
 ![Seaborn Grasshopper](https://i.imgur.com/MUZLQhk.gif)
 Author : Pablo Arango
 
 ## Result examples
+
 ![image](https://user-images.githubusercontent.com/39027094/181036912-2712bd6c-5ec7-4969-8260-b261391a7485.png)
 Author : MaesAntoine
 
+# How to Deploy Your Own Local Server with Seaborn for Grasshopper 3D in 5 minutes
+
+**Requirements**
+
+- Git: Version control is an important tool when working on projects, and Git is one of the most popular and widely-used version control systems. You can download Git [here](https://git-scm.com/downloads).
+- Virtual Environment Manager: This guide uses conda, but you can also use miniconda or other virtual environment managers. You can download conda [here](https://docs.conda.io/en/latest/miniconda.html).
+
+### Step by step guide
+
+1. Clone the repository using the following command:
+
+```bash
+git clone https://github.com/monsieurpablo/seaborn_grasshopper
+```
+
+The command above will download the repository containing the code for the server and its dependencies.
+
+2. Navigate to the repository
+
+```bash
+cd seaborn_grasshopper
+```
+
+This command will take you inside the repository's folder, where you will be able to access the files and folders required to run the server.
+
+1. Create a virtual environment with `Python 3.9` using `conda`
+
+```bash
+conda create --name ghseaborn python=3.9
+conda activate ghseaborn
+```
+
+Conda allows you to create virtual environments, which are isolated spaces where you can install packages and dependencies without interfering with your system's Python installation. The first command creates an environment named ghseaborn with Python 3.10, and the second command activates the environment so that any packages you install will be installed in that environment.
+
+4. Install the dependencies listed in the requirements file:
+
+```bash
+pip install -r requirements.txt
+```
+
+This command will install all the packages and dependencies required to run the server, as listed in the requirements.txt file.
+
+5. Install `waitress` as a server handler (Windows only)
+
+```bash
+pip install waitress
+```
+
+Waitress is a lightweight web server that can be used to run the server. This command installs the waitress package, which is required to run the server on Windows.
+
+6. Start the server
+
+```bash
+waitress-serve --threads 6 --listen=*:5000 app:app
+```
+
+This command starts the server and makes it available on the localhost at port 5000. The --threads flag specifies the number of threads to use and the --listen flag specifies the IP address and port to listen on.
+
+Now you can connect to the server in grasshopper by using the following url address [`http://127.0.0.1:5000/`](http://127.0.0.1:5000/)
+
+7. Connect to the server in Grasshopper
+   Use the the following URL address: http://127.0.0.1:5000/ in the panel asking for the server address.
+
+Congratulations!!!!! You can now connect to the server using the URL address provided and start using Seaborn with Grasshopper 3D.
+
 # Deployment
+
 Follow [this tutorial](https://www.youtube.com/watch?v=SiCAIRc0pEI). The code is already set-up to work with Heroku.
-
-
-
--- all_graphs.py
+++ all_graphs.py
@@ -1,58 +1,66 @@
+import matplotlib
+
+matplotlib.use("Agg")
+
 import base64
-from matplotlib import pyplot as plt
-import pandas as pd
-import seaborn as sns
 import io
 import os
 
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
 # TODO: Change this later
-sns.set_style("ticks") # whitegrid, darkgrid, white, dark, ticks
+sns.set_style("ticks")  # whitegrid, darkgrid, white, dark, ticks
+
 
 def base64img():
     # https://stackoverflow.com/questions/37225035/serialize-in-json-a-base64-encoded-data
     my_stringIObytes = io.BytesIO()
 
-    plt.savefig(my_stringIObytes, format='png', dpi=200)
+    plt.savefig(my_stringIObytes, format="png", dpi=200)
     plt.close()
 
     my_stringIObytes.seek(0)
     imdata_bytes = base64.b64encode(my_stringIObytes.read())
-    imdata_string = imdata_bytes.decode('utf-8')
+    imdata_string = imdata_bytes.decode("utf-8")
 
     my_stringIObytes.flush()
-    
+
     return imdata_string
 
 
 def set_fig_size(g, fig_size):
-    w, h = fig_size.split(';')
+    w, h = fig_size.split(";")
     # try:
     g.figure.set_figwidth(float(w))
     g.figure.set_figheight(float(h))
     # except:
-        
-def check_numeric(df,column):
+
+
+def check_numeric(df, column):
     # try:
     if pd.api.types.is_numeric_dtype(df[column]):
         return True
     else:
         return False
         # raise ValueError(f"{column} does not contain numbers")
-            
+
     # except ValueError as e:
     #     return e
+
 
 def empty2none(args):
     for k, v in args.items():
         if type(v) == str:
-            args[k] = None if v == '' else v
+            args[k] = None if v == "" else v
         if type(v) == int:
             args[k] = None if v == -999 else v
     return args
 
 
 def clean_args(args):
-    rm_list = ['add_args', 'ax_args', 'despine', 'fig_size']
+    rm_list = ["add_args", "ax_args", "despine", "fig_size"]
     # remove optional args
     try:
         [args.pop(k) for k in rm_list]
@@ -60,21 +68,38 @@ def clean_args(args):
         pass
 
     args = empty2none(args)
-    
+
     # TODO check if string represents tuple or list (eval) ?
 
     return args
 
-def example_data(data_base_name='iris'):
+
+def example_data(data_base_name="iris"):
     # https://seaborn.pydata.org/generated/seaborn.load_dataset.html#seaborn.load_dataset
-    sns.load_dataset(data_base_name).to_csv(index=False, line_terminator='@')
+    sns.load_dataset(data_base_name).to_csv(index=False, line_terminator="@")
 
 
 # ---------------------------------
 # RELATIONAL PLOTS
 
 
-def rel(data, x, y, hue, size, style, row, col, col_wrap, kind='scatter', palette='deep', fig_size ='', despine={}, add_args={}, ax_args={}):
+def rel(
+    data,
+    x,
+    y,
+    hue,
+    size,
+    style,
+    row,
+    col,
+    col_wrap,
+    kind="scatter",
+    palette="deep",
+    fig_size="",
+    despine={},
+    add_args={},
+    ax_args={},
+):
     # https://seaborn.pydata.org/generated/seaborn.relplot.html#seaborn.relplot
 
     args = clean_args(locals())
@@ -137,7 +162,10 @@ def line(data, x, y, hue, palette, fig_size, despine={}, add_args={}, ax_args={}
 # -------------------------------------------------
 # DISTRIBUTION PLOTS
 
-def dis(data, x, y, hue, row, col, col_wrap, kind, rug, legend, palette, fig_size, despine={}, add_args={}, ax_args={}):
+
+def dis(
+    data, x, y, hue, row, col, col_wrap, kind, rug, legend, palette, fig_size, despine={}, add_args={}, ax_args={}
+):
 
     # https://seaborn.pydata.org/generated/seaborn.displot.html#seaborn.displot
 
@@ -158,8 +186,25 @@ def dis(data, x, y, hue, row, col, col_wrap, kind, rug, legend, palette, fig_siz
     return base64img()
 
 
-def hist(data, x, y, hue, stat, cumulative, multiple, element, fill, shrink, kde, legend,
-         palette, fig_size, despine={}, add_args={}, ax_args={}):
+def hist(
+    data,
+    x,
+    y,
+    hue,
+    stat,
+    cumulative,
+    multiple,
+    element,
+    fill,
+    shrink,
+    kde,
+    legend,
+    palette,
+    fig_size,
+    despine={},
+    add_args={},
+    ax_args={},
+):
     # https://seaborn.pydata.org/generated/seaborn.histplot.html#seaborn.histplot
 
     args = clean_args(locals())
@@ -179,8 +224,27 @@ def hist(data, x, y, hue, stat, cumulative, multiple, element, fill, shrink, kde
     return base64img()
 
 
-def kde(data, x, y, hue, cut, cumulative, multiple, common_norm, common_grid, levels, thresh,
-        alpha, fill, legend, palette, fig_size, despine={}, add_args={}, ax_args={}):
+def kde(
+    data,
+    x,
+    y,
+    hue,
+    cut,
+    cumulative,
+    multiple,
+    common_norm,
+    common_grid,
+    levels,
+    thresh,
+    alpha,
+    fill,
+    legend,
+    palette,
+    fig_size,
+    despine={},
+    add_args={},
+    ax_args={},
+):
     # https://seaborn.pydata.org/generated/seaborn.kdeplot.html#seaborn.kdeplot
 
     args = clean_args(locals())
@@ -394,23 +458,25 @@ def count(data, x, hue, dodge, palette, fig_size, despine={}, add_args={}, ax_ar
 
     return base64img()
 
+
 # -----------------------
 
-def joint(data, x, y, hue, kind, palette, fig_size,  despine={}, add_args={}, ax_args={}):
+
+def joint(data, x, y, hue, kind, palette, fig_size, despine={}, add_args={}, ax_args={}):
     # https://seaborn.pydata.org/generated/seaborn.jointplot.html#seaborn.jointplot
 
     args = clean_args(locals())
-    
-    # check that x and y columns are numeric 
+
+    # check that x and y columns are numeric
     if sum([check_numeric(data, x) for x in [x, y]]) != 2:
         raise ValueError(f'x column "{x}" and y columns "{y}" must be numeric')
-    
+
     g = sns.jointplot(**args, **add_args)
-    # g.set(**ax_args) # joint plot does not have an ax_args 
+    # g.set(**ax_args) # joint plot does not have an ax_args
 
     if fig_size:
         set_fig_size(g, fig_size)
-  
+
     if despine:
         sns.despine(**despine)
 
@@ -419,8 +485,11 @@ def joint(data, x, y, hue, kind, palette, fig_size,  despine={}, add_args={}, ax
 
     return base64img()
 
+
 # -----------------------
-def pair(data, hue, vars, x_vars, y_vars, palette, kind, diag_kind, corner, fig_size, despine={}, add_args={}, ax_args={}):
+def pair(
+    data, hue, vars, x_vars, y_vars, palette, kind, diag_kind, corner, fig_size, despine={}, add_args={}, ax_args={}
+):
     # https://seaborn.pydata.org/generated/seaborn.pairplot.html#seaborn.pairplot
 
     args = clean_args(locals())
